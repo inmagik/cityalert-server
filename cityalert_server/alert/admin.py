dfe9db6d1c8b2fd2ff74a949a3e7af59b657d5b0
Mauro Bianchi <bianchimro@gmail.com>
2018-09-08 16:51:14 +0200

    data model

--- cityalert_server/alert/admin.py
+++ cityalert_server/alert/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import AlertType, Alert
+from .models import AlertType, Alert, AlertResponse
 
+admin.site.register(AlertResponse)
 admin.site.register(AlertType)
 admin.site.register(Alert)
--- cityalert_server/alert/models.py
+++ cityalert_server/alert/models.py
@@ -1,4 +1,24 @@
 from django.db import models
+from django.conf import settings
+
+
+class AlertResponse(models.Model):
+    """
+    """
+    STATUS_CHOICES = (
+        ('accepted', 'Presa in carico'),
+        ('resolved', 'Risolta'),
+        ('invalid', 'Invalida'),
+    )
+    PRIORITY_CHOICES = (
+        (1, 'Bassa'),
+        (2, 'Media'),
+        (3, 'Alta'),
+    )
+
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default='accepted')
+    security_issue = models.BooleanField(default=False)
+    priority = models.IntegerField(choices=PRIORITY_CHOICES, default=1)
 
 
 class AlertType(models.Model):
@@ -9,8 +29,17 @@ class AlertType(models.Model):
 
 
 class Alert(models.Model):
+
     alert_type = models.ForeignKey(AlertType, models.CASCADE, null=True, blank=True)
     description = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE)
+
+    lat = models.FloatField(blank=True, null=True)
+    lon = models.FloatField(blank=True, null=True)
+    location = models.CharField(blank=True, null=True, max_length=256)
+
+    response = models.ForeignKey(AlertResponse, models.CASCADE, null=True, blank=True, related_name="alerts")
+
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
