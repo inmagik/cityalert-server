df74f982018c47393a780b0adbabfc113734f0e6
gffuma <fumagalli.gf@gmail.com>
2018-09-08 23:56:27 +0200

    Fix vote alert

--- cityalert_server/api/views.py
+++ cityalert_server/api/views.py
@@ -8,9 +8,9 @@ from alert.models import Alert, AlertResponse, AlertVote
 class VoteAlertView(APIView):
     def post(self, request, alert_id):
         alert = Alert.objects.get(pk=alert_id)
-        user = self.request.user
+        user = request.user
         alert_vote = AlertVote.objects.create(alert=alert, user=user)
-        return Response(AlertSerializer(instance=alert).data)
+        return Response(AlertSerializer(instance=alert, context={'request': request}).data)
 
 class AlertViewSet(ModelViewSet):
     serializer_class = AlertSerializer
